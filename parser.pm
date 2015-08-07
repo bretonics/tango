a681f99a4c2dca6a506ddffcbcd0a38c7ddab9af
Andres Breton <bretonics@users.noreply.github.com>
2015-08-06 23:21:57 -0400

    divided parseHeader to subs => return all values to main script

--- parser.pm
+++ parser.pm
@@ -15,28 +15,55 @@ use Carp;
 # =============================================
 
 
-parseHeader($NCBIfile);
 sub parseHeader {
     my ($NCBIfile) = @_;
-    open(FILE, "<", $NCBIfile);
-
-    while(<FILE>) {
-        # Get Locus Name and Sequence Length
-        if($_ =~ /^LOCUS\s+(\w+)\s+(\d+)\s+/) {
-            my ($locus, $seqLen) = ($1, $2);
-        }
-        # Get Accession
-        if($_ =~ /^ACCESSION\s+(\w+)/) {
-            my $accession = $1;
-        }
-        # Get Version
-        if($_ =~ /^VERSION\s+(\w+)\s+/) {
-            my $version = $1;
-        }
-        # Get Organism
-        if($_ =~ /organism="(.*?)"/) {
-            my $organism = $1;
-        }
+    unless(open(INFILE, "<", $NCBIfile)) {
+        croak "Can't open $NCBIfile for reading " , $!;
     }
-    close(FILE);
+
+    ## Get entire file in variable
+    $/ = ''; #line separator
+    my $FILE = <INFILE>;
+    $/ = "\n";  #set back line separator
+    close INFILE;
+
+    # Get Locus Name and Sequence Length
+    my ($locus, $seqLen) = getLocus($FILE);
+    # Get Accession
+    my $accession = getAccession($FILE);
+    # Get Version
+    my $version = getVersion($FILE);
+    # Get Organism
+    my $organism = getOrganism($FILE);
+
+    return $locus, $seqLen, $accession, $version, $organism;
+}
+
+sub getLocus {
+    my ($file) = @_;
+    if($file =~ /^LOCUS\s+(\w+)\s+(\d+)\s+/) {
+        my ($locus, $seqLen) = ($1, $2);
+        return $locus, $seqLen;
+    }
+}
+
+sub getAccession {
+    my ($file) = @_;
+    if($file =~ /^ACCESSION\s+(\w+)/m) {
+        my $accession = $1; return $accession;
+    }
+}
+
+sub getVersion {
+    my ($file) = @_;
+    if($file =~ /^VERSION\s+(\w+)\s+/m) {
+            my $version = $1; return $version;
+        }
+}
+
+sub getOrganism {
+    my ($file) = @_;
+    if($file =~ /organism="(.*?)"/) {
+        my $organism = $1; return $organism;
+    }
 }
