640c4349054ce9e04e7be93ab7b08e2f9cbfb379
Andres Breton <bretonics@users.noreply.github.com>
2015-08-08 18:27:16 -0400

    loop multiple IDs added

--- ncbiScrpR.pl
+++ ncbiScrpR.pl
@@ -7,7 +7,7 @@ use File::Basename qw(dirname);
 use Cwd qw(abs_path);
 use lib (dirname abs_path $0). "/lib";
 
-use Eutil; use Parser;
+use Eutil; use Parser; use Database;
 
 
 # =============================================
@@ -20,18 +20,25 @@ use Eutil; use Parser;
 # =============================================
 
 
-#-------------------------------------------------------------------------
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# VARIABLES
+my $outDir = createDownloadDir();
+my $email = 'breton.a@husky.neu.edu';
+my ($NCBIfile, $NCBIstatus);
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # COMMAND LINE
-my $ID = "";
+my @IDS;
+my $FILE = "";
 my $DATABASE = "";
-my $TYPE = "";
-my $FORCE = "";
+my $TYPE = "gb";
+my $FORCE = "0"; #default- Not force
 my $SQLDB = "";
 my $usage= "\n\n $0 [options]\n
 Options:
-    -id    IDs
-    -db     Database (Nucleotide, protein, etc..)
-    -type   fasta, gb, etc...
+    -ids    IDs
+    -file   File with IDS [optional]
+    -db     Database (Nucleotide, protein, etc..) [optional]
+    -type   gb, fasta etc... [optional]
     -force  Force download? [optional]
     -sql    SQL database name
     -help   Shows this message
@@ -39,39 +46,36 @@ Options:
 
 # OPTIONS
 GetOptions(
-    'id=i'      =>\$ID,
+    'id=i{1,}'  =>\@IDS,
+    'file:s'    =>\$FILE,
     'db=s'      =>\$DATABASE,
-    'type=s'    =>\$TYPE,
-    'force:0'   =>\$FORCE, #defaults to NO
+    'type:s'    =>\$TYPE,
+    'force:1'   =>\$FORCE,
     'sql:s'     =>\$SQLDB,
     help        =>sub{pod2usage($usage);}
 )or pod2usage(2);
-#-------------------------------------------------------------------------
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # CHECKS
-unless($ID) {
+unless(@IDS) {
     die "Did not provide an ID, -id 34577062", $!, $usage;
 }
 unless($DATABASE) {
     die "Did not provide a database, -db nucleotide", $!, $usage;
 }
-unless($TYPE) {
-    die "Did not provide a type format, -type fasta", $!, $usage;
-}
+# unless($TYPE) {
+#     die "Did not provide a type format, -type gb", $!, $usage;
+# }
 $SQLDB = "NCBIdatabase" unless $SQLDB ne ""; #default SQL database name
-$FORCE = 0 unless $FORCE != 0; #default force download
-#-------------------------------------------------------------------------
-# VARIABLES
-my $outDir = createDownloadDir();
-my $email = 'breton.a@husky.neu.edu';
-my ($NCBIfile, $NCBIstatus);
-#-------------------------------------------------------------------------
-# CALLS
-# my $temp = join(",", @IDS);
-($NCBIfile, $NCBIstatus) = getNCBIfile($ID, $outDir, $FORCE, $DATABASE, $TYPE, $email);
-if($NCBIstatus == 1) {
-    parseFile($NCBIfile, $TYPE);
-}else {
-    die "Something happened. Could not get file from NCBI", $!;
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# CALLSs
+foreach my $id (@IDS) {
+    ($NCBIfile, $NCBIstatus) = getNCBIfile($id, $outDir, $FORCE, $DATABASE, $TYPE, $email);
+    if($NCBIstatus != 1) {
+        die "Something happened. Could not get file from NCBI", $!;
+    }else {
+        say "Getting NCBI file header content...";
+        parseFile($NCBIfile, $TYPE);
+    }
 }
 
 # Store in SQL database
@@ -81,12 +85,12 @@ if($NCBIstatus == 1) {
 # }else {
 #   printf "Command exited with value %d", $? >> 8;
 # }
-#-------------------------------------------------------------------------
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # SUBS
 sub parseFile {
     my ($NCBIfile, $TYPE) = @_;
     if ($TYPE eq "gb") {
-        my ($locus, $seqLen, $accession, $version, $organism, $sequence) = parseHeader($NCBIfile); say $sequence;
+        my ($locus, $seqLen, $accession, $version, $organism, $sequence, $gene, $proteinID) = parseHeader($NCBIfile);
     }
 }
 
